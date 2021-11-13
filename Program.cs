3a5822efb7cb92a0e11c18195b10de52db3eedbc
SARATHCHANDRA <sarathchandra76@gmail.com>
2021-11-13 10:47:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DELEGATE_VS2019_SAMPLE2
+{
+    public delegate void MyDelegate();
+    class Program
+    {
+        static void Main()
+        {
+            Function();
+            MyDelegate del = new MyDelegate(Function);
+            del.Invoke();
+            MyDelegate del2 = fun;
+            del2();
+        }
+        static void Function()
+        {
+            Console.WriteLine("DELEGATE1");
+        }
+        static void fun()
+        {
+            Console.WriteLine("fun() method");
+        }
+    }
+}
